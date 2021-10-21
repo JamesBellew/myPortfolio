f66cb22fa06324480c79b26dd4fa873502ab2c7f
JamesBellew <47107576+JamesBellew@users.noreply.github.com>
2021-10-21 21:13:33 +0100

    js file

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+
+const show_project_div = document.getElementById('show_project');
+const view_project_btn = document.getElementById('project_button');
+const show_project_exit = document.getElementById('exit_project_btn');
+
+function viewProject(){
+ if(document.getElementById('show_project').display= 'none'){
+        show_project_div.style.display= 'block';
+ }
+
+}
+
+function exit_project(){
+           show_project_div.style.display= 'none';
+   }
